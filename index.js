70f77fe88383aef792db53e9e01d51ddd0c51651
Rahat393 <rahataminulislam37@gmail.com>
2024-01-16 19:30:09 +0600

    delete implement

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.of1ofap.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+ 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -56,8 +56,21 @@ async function run() {
     });
 
     app.get('/bookings', async (req, res) => {
-      console.log(req.query);
-      const result = await bookingsCollection.find().toArray();
+      console.log(req.query.email);
+      let query ={};
+      if(req.query?.email){
+        query = {
+          email : req.query.email
+        }
+      }
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result)
+    });
+
+    app.delete('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingsCollection.deleteOne(query);
       res.send(result)
     })
 
